c0b1e8298ee4811b477afd68aa6639852aa67b25
Wilcosource <nandinandhini98@gmail.com>
2020-03-25 19:32:57 +0530

    Get the leave detail to display in calender view

---
+++ app/src/main/java/com/android/wilcoconnect/model/leave/LeaveCalender.java
@@ -0,0 +1,52 @@
+package com.android.wilcoconnect.model.leave;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class LeaveCalender {
+
+    @SerializedName("code")
+    public int code;
+
+    @SerializedName("Status")
+    public String Status;
+
+    @SerializedName("message")
+    public String message;
+
+    @SerializedName("data")
+    public ArrayList<ApproveList> data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public ArrayList<ApproveList> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<ApproveList> data) {
+        this.data = data;
+    }
+}

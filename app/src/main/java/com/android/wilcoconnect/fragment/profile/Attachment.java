479f94f7bec3c2c2c7dcd30358a9c4026c936d31
Wilcosource <nandinandhini98@gmail.com>
2020-04-10 12:35:50 +0530

    Attachments

--- app/src/main/java/com/android/wilcoconnect/fragment/profile/Attachment.java
+++ app/src/main/java/com/android/wilcoconnect/fragment/profile/Attachment.java
@@ -6,15 +6,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.text.style.IconMarginSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.android.wilcoconnect.R;
 import com.android.wilcoconnect.api.ApiManager;

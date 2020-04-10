9075ee9f6a6649b0d99e8baa410f4b4a6ee1c274
Wilcosource <nandinandhini98@gmail.com>
2020-04-10 16:41:39 +0530

    Attachments details displayed success

--- app/src/main/java/com/android/wilcoconnect/fragment/profile/Attachment.java
+++ app/src/main/java/com/android/wilcoconnect/fragment/profile/Attachment.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.android.wilcoconnect.R;
 import com.android.wilcoconnect.api.ApiManager;
@@ -33,11 +35,41 @@ public class Attachment extends DialogFragment {
     private AddRequest addRequest = new AddRequest();
     private AttachmentDetails attachmentDetail = new AttachmentDetails();
     private AttachmentDetailData attachmentDetailData = new AttachmentDetailData();
+    private String[] adharCard;
+    private String[] panCard;
+    private String[] presentAddress;
+    private String[] dob;
+    private String[] passport;
+    private String[] edu1_degree;
+    private String[] edu1_provisional;
+    private String[] edu1_marksheet;
+    private String[] edu2_degree;
+    private String[] edu2_provisional;
+    private String[] edu2_marksheet;
+    private String[] experience_certificate;
+    private String[] payslips;
+    private String[] Experience;
+    private LinearLayout adharCard_layout,panCard_layout,presentAddress_layout,dob_layout,passport_layout,edu1degree_layout,edu1provisional_layout,edu1marksheet_layout,edu2degree_layout,edu2provisional_layout,edu2marksheet_layout,experiencecertificate_layout,payslips_layout,experience_layout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_attachment, container, false);
+
+        adharCard_layout = view.findViewById(R.id.et_View_aadhar);
+        panCard_layout = view.findViewById(R.id.et_View_pan);
+        presentAddress_layout = view.findViewById(R.id.et_View_addressproof);
+        dob_layout = view.findViewById(R.id.et_View_dob);
+        passport_layout = view.findViewById(R.id.et_View_passport);
+        edu1degree_layout = view.findViewById(R.id.et_View_edu1);
+        edu1provisional_layout = view.findViewById(R.id.et_View_prov);
+        edu1marksheet_layout = view.findViewById(R.id.et_View_marksheet);
+        edu2degree_layout = view.findViewById(R.id.et_View_degree);
+        edu2provisional_layout = view.findViewById(R.id.et_View_provcert);
+        edu2marksheet_layout = view.findViewById(R.id.et_View_Marks);
+        experiencecertificate_layout = view.findViewById(R.id.et_View_exper);
+        payslips_layout = view.findViewById(R.id.et_View_payslip);
+        experience_layout = view.findViewById(R.id.et_View_experience);
 
         /*
          * Get the Header
@@ -74,7 +106,8 @@ public class Attachment extends DialogFragment {
                 attachmentDetail = response.body();
                 if(attachmentDetail!=null && response.isSuccessful()){
                     attachmentDetailData = attachmentDetail.getData().get(0).getObjAttachement();
-                }
+                    get_array_data();
+            }
             }
             /*
              * Api Call Failure
@@ -86,6 +119,170 @@ public class Attachment extends DialogFragment {
         });
 
         return view;
+    }
+
+    private void get_array_data() {
+        for (int i = 0; i < attachmentDetailData.getDocumentList().size(); i++) {
+            if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Aadhar Card")) {
+                adharCard = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    adharCard[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("PAN Card")) {
+                panCard = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    panCard[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Present Address Proof")) {
+                presentAddress = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int b = 0; b < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); b++) {
+                    presentAddress[b] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(b);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Proof of Date of Birth")) {
+                dob = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    dob[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Passport")) {
+                passport = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    passport[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Education1 - Degree")) {
+                edu1_degree = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    edu1_degree[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Education1 - Provisional Certificate")) {
+                edu1_provisional = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    edu1_provisional[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Education1 - Marksheets")) {
+                edu1_marksheet = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    edu1_marksheet[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Degree")) {
+                edu2_degree = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    edu2_degree[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Provisional Certificate")) {
+                edu2_provisional = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    edu2_provisional[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Marksheets")) {
+                edu2_marksheet = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    edu2_marksheet[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Experience Certificate / Relieving Letter from Previous Employment ")) {
+                experience_certificate = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    experience_certificate[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Payslips")) {
+                payslips = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    payslips[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            } else if (attachmentDetailData.getDocumentList().get(i).getAttachmentFileName().equals("Experience")) {
+                Experience = new String[attachmentDetailData.getDocumentList().get(i).getFilelist().size()];
+                for (int a = 0; a < attachmentDetailData.getDocumentList().get(i).getFilelist().size(); a++) {
+                    Experience[a] = attachmentDetailData.getDocumentList().get(i).getFilelist().get(a);
+                }
+            }
+        }
+        set_attachment();
+    }
+
+    private void set_attachment() {
+        for (int i=0; i<adharCard.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            adharCard_layout.addView(imageView);
+        }
+        for (int i=0; i<panCard.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            panCard_layout.addView(imageView);
+        }
+        for (int i=0; i<presentAddress.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            presentAddress_layout.addView(imageView);
+        }
+        for (int i=0; i<passport.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            passport_layout.addView(imageView);
+        }
+        for (int i=0; i<dob.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            dob_layout.addView(imageView);
+        }
+        for (int i=0; i<edu1_degree.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            edu1degree_layout.addView(imageView);
+        }
+        for (int i=0; i<edu1_provisional.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            edu1provisional_layout.addView(imageView);
+        }
+        for (int i=0; i<edu1_marksheet.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            edu1marksheet_layout.addView(imageView);
+        }
+        for (int i=0; i<edu2_degree.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            edu2degree_layout.addView(imageView);
+        }
+        for (int i=0; i<edu2_provisional.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            edu2provisional_layout.addView(imageView);
+        }
+        for (int i=0; i<edu2_marksheet.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            edu2marksheet_layout.addView(imageView);
+        }
+        for (int i=0; i<experience_certificate.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            experiencecertificate_layout.addView(imageView);
+        }
+        for (int i=0; i<payslips.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            payslips_layout.addView(imageView);
+        }
+        for (int i=0; i<Experience.length;i++){
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+            imageView.setImageResource(R.drawable.attachment);
+            experience_layout.addView(imageView);
+        }
     }
 
     @Override

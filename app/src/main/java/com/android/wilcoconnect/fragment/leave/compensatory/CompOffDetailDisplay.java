febf75ad37395bdce0a153eeb0f77835fd0fba5d
Wilcosource <nandinandhini98@gmail.com>
2020-04-15 13:01:34 +0530

    change the Design for Attachment

--- app/src/main/java/com/android/wilcoconnect/fragment/leave/compensatory/CompOffDetailDisplay.java
+++ app/src/main/java/com/android/wilcoconnect/fragment/leave/compensatory/CompOffDetailDisplay.java
@@ -18,15 +18,15 @@ public class CompOffDetailDisplay extends DialogFragment {
 
     public static final String TAG = "CompOffDetailDisplay";
     private CompOffDetailData data;
-    private TextView tv_View_fromdate;
-    private TextView tv_View_todate;
+    private TextView tv_View_fromDate;
+    private TextView tv_View_toDate;
     private TextView tv_View_no_of_counts;
     private TextView tv_View_remarks;
-    private TextView tv_View_leavestatus;
-    private TextView tv_View_approveddate;
-    private TextView tv_View_remarksbyapprover;
+    private TextView tv_View_leaveStatus;
+    private TextView tv_View_approvedDate;
+    private TextView tv_View_remarksByApprover;
     private TextView tv_View_approver;
-    private TextView tv_View_applieddate;
+    private TextView tv_View_appliedDate;
     private TextView label1,label2,label3;
 
     @Override
@@ -47,36 +47,36 @@ public class CompOffDetailDisplay extends DialogFragment {
         detail_toolbar.setNavigationIcon(R.drawable.close);
         detail_toolbar.setNavigationOnClickListener(v -> dismiss());
 
-        tv_View_applieddate = view.findViewById(R.id.et_View_applieddate);
-        tv_View_fromdate = view.findViewById(R.id.et_View_fromdate);
-        tv_View_todate = view.findViewById(R.id.et_View_todate);
+        tv_View_appliedDate = view.findViewById(R.id.et_View_applieddate);
+        tv_View_fromDate = view.findViewById(R.id.et_View_fromdate);
+        tv_View_toDate = view.findViewById(R.id.et_View_todate);
         tv_View_no_of_counts = view.findViewById(R.id.et_View_no_of_counts);
         tv_View_remarks = view.findViewById(R.id.et_View_remarks);
-        tv_View_leavestatus = view.findViewById(R.id.et_View_status);
-        tv_View_approveddate = view.findViewById(R.id.et_View_approveddate);
-        tv_View_remarksbyapprover = view.findViewById(R.id.et_View_remarksbyapprover);
+        tv_View_leaveStatus = view.findViewById(R.id.et_View_status);
+        tv_View_approvedDate = view.findViewById(R.id.et_View_approveddate);
+        tv_View_remarksByApprover = view.findViewById(R.id.et_View_remarksbyapprover);
         tv_View_approver = view.findViewById(R.id.et_View_approver);
 
         label1 = view.findViewById(R.id.tv_View_approveddate);
         label2 = view.findViewById(R.id.tv_View_remarksbyapprover);
         label3 = view.findViewById(R.id.tv_View_approver);
 
-        tv_View_applieddate.setText(data.getAppliedDate());
-        tv_View_fromdate.setText(data.getStrFromDate());
-        tv_View_todate.setText(data.getStrToDate());
+        tv_View_appliedDate.setText(data.getAppliedDate());
+        tv_View_fromDate.setText(data.getStrFromDate());
+        tv_View_toDate.setText(data.getStrToDate());
         tv_View_no_of_counts.setText(""+data.getNoOfDays());
         tv_View_remarks.setText(data.getRequestRemarks());
-        tv_View_leavestatus.setText(data.getRequestStatus());
+        tv_View_leaveStatus.setText(data.getRequestStatus());
         if(data.getRequestStatus().equals("Applied")){
-            tv_View_approveddate.setVisibility(View.GONE);
-            tv_View_remarksbyapprover.setVisibility(View.GONE);
+            tv_View_approvedDate.setVisibility(View.GONE);
+            tv_View_remarksByApprover.setVisibility(View.GONE);
             tv_View_approver.setVisibility(View.GONE);
             label1.setVisibility(View.GONE);
             label2.setVisibility(View.GONE);
             label3.setVisibility(View.GONE);
         }
-        tv_View_approveddate.setText(data.getApprovedDate());
-        tv_View_remarksbyapprover.setText(data.getApprovedRemarks());
+        tv_View_approvedDate.setText(data.getApprovedDate());
+        tv_View_remarksByApprover.setText(data.getApprovedRemarks());
         tv_View_approver.setText(data.getPrimaryApprover());
 
         return view;
